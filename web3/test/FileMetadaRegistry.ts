ca7aaedc7761165978a330dc31b07e4465c591a5
Carlos Olivo <carlosengel011@gmail.com>
2024-04-13 21:01:39 -0400

    Refactor FileMetadataRegistry tests and add delete functions

--- web3/test/FileMetadaRegistry.ts
+++ web3/test/FileMetadaRegistry.ts
@@ -35,16 +35,80 @@ describe("FileMetadataRegistry", function () {
       const fileName = "file.txt";
       const userId = ethers.encodeBytes32String("0x456");
       const size = 100;
-      const key = ethers.encodeBytes32String("storageKey");
+      const key = "storageKey";
 
       const tx = await fileMetadataRegistry
         .connect(owner)
         .registerFile(key, fileChecksum, fileName, size, userId);
       await tx.wait();
+    });
+  });
+
+  describe("getFilesByUser", function () {
+    it("Should return the right metadata", async function () {
+      const { fileMetadataRegistry, owner } = await loadFixture(
+        deployFileMetadataRegistryFixture
+      );
+
+      const fileChecksum = "0x1234567890abcdefffff";
+      const fileName = "file.txt";
+      const userId = ethers.encodeBytes32String("0x456");
+      const size = 100;
+      const key = "storageKey";
+
+      await fileMetadataRegistry
+        .connect(owner)
+        .registerFile(key, fileChecksum, fileName, size, userId);
 
       const metadata = await fileMetadataRegistry.getFilesByUser(userId);
       expect(metadata.length).to.equal(1);
       expect(metadata[0].checksum).to.equal(fileChecksum);
+    });
+  });
+
+  describe("deleteUserFileAt", function () {
+    it("Should delete the file at the right index", async function () {
+      const { fileMetadataRegistry, owner } = await loadFixture(
+        deployFileMetadataRegistryFixture
+      );
+
+      const fileChecksum = "0x1234567890abcdefffff";
+      const fileName = "file.txt";
+      const userId = ethers.encodeBytes32String("0x456");
+      const size = 100;
+      const key = "storageKey";
+
+      await fileMetadataRegistry
+        .connect(owner)
+        .registerFile(key, fileChecksum, fileName, size, userId);
+
+      await fileMetadataRegistry.connect(owner).deleteUserFileAt(userId, 0);
+
+      const metadata = await fileMetadataRegistry.getFilesByUser(userId);
+      expect(metadata.length).to.equal(0);
+    });
+  });
+
+  describe("deleteAllUserFiles", function () {
+    it("Should delete the file", async function () {
+      const { fileMetadataRegistry, owner } = await loadFixture(
+        deployFileMetadataRegistryFixture
+      );
+
+      const fileChecksum = "0x1234567890abcdefffff";
+      const fileName = "file.txt";
+      const userId = ethers.encodeBytes32String("0x456");
+      const size = 100;
+      const key = "storageKey";
+
+      await fileMetadataRegistry
+        .connect(owner)
+        .registerFile(key, fileChecksum, fileName, size, userId);
+
+      await fileMetadataRegistry.connect(owner).deleteAllUserFiles(userId);
+
+      const metadata = await fileMetadataRegistry.getFilesByUser(userId);
+      expect(metadata.length).to.equal(0);
     });
   });
 });
