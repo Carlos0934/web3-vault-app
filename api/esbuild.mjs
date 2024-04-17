fbcd300efa115c45e79eae159e9db27096e05a4f
Carlos Olivo <carlosengel011@gmail.com>
2024-04-16 21:51:23 -0400

    fix esbuild

--- api/esbuild.mjs
+++ api/esbuild.mjs
@@ -2,13 +2,11 @@ import * as esbuild from 'esbuild'
 
 await esbuild.build({
 
-  bundle: true,
   minify: true,
   outdir: './dist',
   platform: 'node',
   target: 'node20',
   entryPoints: ['./src/function.ts'],
-  external: ['aws-sdk'],
   loader: {
       '.node' : 'binary'
   }
