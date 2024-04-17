6d02b2a7ed81de69e04bb35a92530d63966e8310
Carlos Olivo <carlosengel011@gmail.com>
2024-04-16 21:53:56 -0400

    Update esbuild configuration to bundle and minify code

--- api/esbuild.mjs
+++ api/esbuild.mjs
@@ -1,13 +1,14 @@
 import * as esbuild from 'esbuild'
 
 await esbuild.build({
-
+  bundle: true,
   minify: true,
   outdir: './dist',
   platform: 'node',
   target: 'node20',
   entryPoints: ['./src/function.ts'],
   loader: {
-      '.node' : 'binary'
+      '.node' : 'binary',
+
   }
 })
